e238719aa28a5d10633a0fbc440ac12bdc471d4c
SYED TAYYAB SAGHEER <syedtayyabsagheer@gmail.com>
2026-05-20 15:29:15 +0500

    save meal into the database

---
+++ components/meals/share-meal-form.js
@@ -0,0 +1,44 @@
+'use client';
+
+import ImagePicker from '@/components/meals/image-picker';
+import classes from '@/app/meals/share/page.module.css';
+
+export default function ShareMealForm({ shareMeal }) {
+  return (
+    <main className={classes.main}>
+      <form className={classes.form} action={shareMeal}>
+        <div className={classes.row}>
+          <p>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" name="name" required />
+          </p>
+          <p>
+            <label htmlFor="email">Your email</label>
+            <input type="email" id="email" name="email" required />
+          </p>
+        </div>
+        <p>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" required />
+        </p>
+        <p>
+          <label htmlFor="summary">Short Summary</label>
+          <input type="text" id="summary" name="summary" required />
+        </p>
+        <p>
+          <label htmlFor="instructions">Instructions</label>
+          <textarea
+            id="instructions"
+            name="instructions"
+            rows="10"
+            required
+          ></textarea>
+        </p>
+        <ImagePicker label="Your image" name="image" />
+        <p className={classes.actions}>
+          <button type="submit">Share Meal</button>
+        </p>
+      </form>
+    </main>
+  );
+}
